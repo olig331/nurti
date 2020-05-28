1e86a4f895003345f420443e7b5b210b9a479ef4
olig331 <61230543+olig331@users.noreply.github.com>
2020-05-28 18:38:49 +0100

    Started on Set Up Page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import {Search} from './components/search';
+import {SetUpPage} from './components/setUpPage'
 import './style.css';
 
 
 const App = () =>{
   return(
     <div className="App">
-      <Search />
+      {/* <Search /> */}
+      <SetUpPage />
     </div>
   )
 }
--- src/components/setUpPage.js
+++ src/components/setUpPage.js
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {FaRegArrowAltCircleRight, FaFemale} from 'react-icons/fa'
+import {IoIosMan} from 'react-icons/io'
 
 export const SetUpPage = () => {
+  
+  const [complete, setcomplete] = useState(false)
+  const [arrowClicked, setarrowClicked] = useState(false)
+
   return (
-    <div>
-      
+    <div className="set_up_parent">
+        <div className="welcome">
+          <h2>Welcome to set up</h2>
+          <p>please click the arrow to continue</p>
+          <span ><FaRegArrowAltCircleRight /></span>
+        </div>
+        <div className="personal_info">
+          <div>
+            <h4>Select Gender:</h4>
+            <span><IoIosMan /></span>
+            <span><FaFemale /></span>
+          </div>
+            <div className="age">
+              <h4>Enter Age:</h4>
+                <input type="text" placeholder="Age..."/>
+            </div>
+              <div className="weight">
+                <h4>Enter Weight in KG</h4>
+                  <button type="radio">KG</button>
+                  <button type="radio">LBs</button><br></br>
+                  <input type="text" placeholder="enter..."/>
+              </div>
+        </div>
     </div>
   )
 }
